f4e5dfd10c046faaabf44404aceb5ac52ced54e8
tejesh-eds <que18815@adobe.com>
2024-09-23 20:39:10 +0530

    build issues

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -124,7 +124,6 @@ export default async function decorate(block) {
     const section = nav.children[i];
     if (section) section.classList.add(`nav-${c}`);
   });
-  
   const navBrand = nav.querySelector('.nav-brand');
   const brandLink = navBrand.querySelector('.button');
   if (brandLink) {
@@ -165,5 +164,5 @@ export default async function decorate(block) {
   block.append(navWrapper);
 
   const signInWrapper = nav.querySelector('.nav-sign-in');
-  block.prepend(signInWrapper)
+  block.prepend(signInWrapper);
 }
